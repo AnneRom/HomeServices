dd6369495ea35fef42ed99c38950f41560c7862b
AnneRom <coolann000.com@gmail.com>
2024-11-04 17:36:31 +0200

    Lesson_31-10
    Доробили бокове меню.

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.getElementById('menuToggle');
     const menuBody = document.getElementById('menuBody');
+    const overlay = document.getElementById('overlay');
 
     menuToggle.addEventListener('click', () => {
         menuBody.classList.toggle('active');
         menuToggle.classList.toggle('open'); // змінюємо клас іконки для заміни на хрестик
+        overlay.classList.toggle('active');
     });
+
+    window.addEventListener ('resize', () => {
+        if (window.innerWidth > 767.98) {
+            menuBody.classList.remove('active');
+            menuToggle.classList.remove('open');
+            menuBody.classList.add("no-transition");
+        } else {
+            menuBody.classList.remove("no-transition");
+        }
+    })
 });
