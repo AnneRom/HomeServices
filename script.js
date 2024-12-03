a7c171e67a543d3cb91bae834b109e1398cc26d8
AnneRom <coolann000.com@gmail.com>
2024-12-03 18:34:12 +0200

    Lesson_03-12
    Інтерактивний хедер та розмітку для секції services.

--- script.js
+++ script.js
@@ -33,13 +33,9 @@ document.addEventListener('DOMContentLoaded', () => {
         let scrollTop = window.scrollY || document.documentElement.scrollTop;
         if ((scrollTop > lastScrollTop) && (scrollTop > header.offsetHeight)) {
             header.classList.add('hidden');
-            header.classList.add('dark');
         } else if (scrollTop < lastScrollTop) {
             header.classList.remove('hidden');
         } 
-        if (scrollTop < header.offsetHeight) {
-            header.classList.remove('dark');
-        }
         
         lastScrollTop = scrollTop;
 });
